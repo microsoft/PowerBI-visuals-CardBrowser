b6756ff56098a3d5bbe55e699e033e2c36b7923c
Peter MacMurchy <pmacmurchy@uncharted.software>
2018-02-26 16:35:50 -0500

    [LICENSE] Consistently apply Apache-2.0 [DATA] Don't create documents with null IDs and don't create invalid selection IDs (which come from loadMoreData only generating keys for rows included in the initial load)

--- src/dataConversion.ts
+++ src/dataConversion.ts
@@ -1,25 +1,19 @@
-/**
- * Copyright (c) 2017 Uncharted Software Inc.
- * http://www.uncharted.software/
+/*
+ * Copyright 2018 Uncharted Software Inc.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the 'Software'), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 import DataView = powerbi.DataView;
 import IColorInfo = powerbi.IColorInfo;
 import IVisualHost = powerbi.extensibility.v120.IVisualHost;
@@ -40,10 +34,14 @@ function flattenMetaData(metaData) {
 
 function createSelectionId(i, dataView: DataView, host) {
     const category = dataView.categorical.categories && dataView.categorical.categories[0];
+    if (category.identity[i].key) {
+        return new powerbi.visuals.SelectionIdBuilder()
+            .withCategory(category, i)
+            .withMeasure(dataView.metadata.columns.find((col) => col.roles.id).queryName)
+            .createSelectionId();
+    }
 
-    return new powerbi.visuals.SelectionIdBuilder()
-        .withCategory(category, i)
-        .createSelectionId();
+    return null;
 }
 
 function convertToDocuments(rowObjs, dataView, host) {
@@ -57,7 +55,7 @@ function convertToDocuments(rowObjs, dataView, host) {
     for (i = 0; i < rowCount; i++) {
         obj = rowObjs[i];
         docId = obj.id;
-        if (!documents[docId]) {
+        if (docId !== null && !documents[docId]) {
             obj.selectionId = createSelectionId(i, dataView, host);
             documents[docId] = obj;
             documentList.push(documents[docId]);
